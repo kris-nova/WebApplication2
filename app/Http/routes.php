e7ed8e8a52b2eae21745896db5bb1f3b5961d17e
sabio-designs <sabio@sabiodesigns.com>
2016-01-04 20:31:19 -0500

    Adding in Template for Audio Test
    adding in template, routed it in as well.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,12 +15,56 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*upload page for testing variables*/
+Route::get('audio-test', function () {
+    //testing out this hello page
+    return view('audio-test');
+});
+
+/*basic view pages*/
+Route::get('hello', function () {
+    //testing out this hello page
+    echo 'hello page';
+});
+
+Route::get('about', function () {
+    //about page
+    echo 'about page';
+});
+
+Route::get('contact', function () {
+    //contact
+    echo 'contact page';
+});
+
+Route::get('support', function () {
+    //support
+    echo 'support page';
+});
+
+
+/* auth goodness*/
 Route::get('login', function () {
     //do some login auth stuff
     echo 'login page';
 });
+/*
+Route::get('https://devapi.soundeavor.com/User/Auth/Login/Facebook', function () {
+    //do some login auth stuff
+    echo 'hello'. facebookAccessToken_text;
+});
+*/
 
 Route::get('logout', function () {
     //do some login auth stuff
     echo 'logout page';
 });
+
+
+/*IF logged in as a sponsor user type*/
+Route::get('profile', function () {
+    //clearly need to add in the variable for each profile in the get request
+    echo 'hello page';
+});
+
+/*If logged in as a musician user type*/
